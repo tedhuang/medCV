a9d4cbf9f23ea99a8b8d5f049d4e5527b82cfba6
crysng <crystalng.w@gmail.com>
2014-06-02 15:09:00 -0700

    graph

--- medCV/report/static/js/report.js
+++ medCV/report/static/js/report.js
@@ -16,8 +16,71 @@ var  medCV = function () {
 
     function initialize(){
         $(".graph").width(viewportWidth-labelWidth-5); //set graph width
+
+        //TODO: load data
+
+        //graph
+        var sleep =[["1999-05-01", 14], ["2004-05-01", 30], ["2007-05-01", 45], ["2009-05-01", 84], ["2012-05-01", 84]];
+
+        var med =[["1999-05-01", 0], ["2004-05-01", 1], ["2007-05-01", 3], ["2009-05-01", 3], ["2012-05-01", 1]];
+
+        drawGraph("sleep", sleep);
+        drawGraph("medication", med);
+
     }
 
+    function drawGraph(placeholder, rawData){
+        var min = rawData[0][0];
+        $.each(rawData, function(i, data){
+            data[0] = convertDateToTimestamp(data[0]);
+        });
+        var data = [
+            { label: placeholder, data: rawData, points: { fillColor: "#4572A7", size: 5 }, color: '#4572A7' }
+        ];
+        var options ={
+          xaxis: {
+                min: convertDateToTimestamp(min),
+                max: (new Date(2013, 11, 1)).getTime(),
+                mode: "time",
+                tickSize: [1, "year"],
+
+                tickLength: 0,
+                axisLabel: 'Month',
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+                axisLabelPadding: 5
+            },
+            yaxis: {
+                axisLabelUseCanvas: true,
+                axisLabelFontSizePixels: 12,
+                axisLabelFontFamily: 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+                axisLabelPadding: 5
+            },
+            series: {
+                lines: {
+                    show: true,
+                    fill: true,
+                    steps: true
+                }
+            },
+            grid: {
+                borderWidth: 1
+            },
+            legend: {
+                labelBoxBorderColor: "none",
+                position: "right"
+            }
+        };
+        var plot = $("."+placeholder+" > .graph").plot(data, options).data("plot");
+
+    }
+
+    function convertDateToTimestamp(myDate){
+        myDate=myDate.split("-");
+        var newDate=myDate[1]+"/"+myDate[2]+"/"+myDate[0];//month/date year
+        return new Date(newDate).getTime();
+    }
 
     $(function() {
         initialize();
